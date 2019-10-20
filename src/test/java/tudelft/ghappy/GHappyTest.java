1af74598f1bf9ce367ea3d4344b697b60fd61ea4
Kalman.Farkas <44946141+kalmii@users.noreply.github.com>
2019-10-20 13:22:42 +0200

    GHappy quiz is done
    Test are now added to Week 2 GHappy exercise

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,80 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GHappyTest {
+    private GHappy test;
+    private boolean actual;
+
+    @BeforeEach
+    public void setup(){
+        test = new GHappy();
+        actual = false;
+    }
+
+    @Test
+    public void testGHappyEmptyString(){
+        actual = test.gHappy("");
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testGHappyNotEmptyStringWithoutG(){
+        actual = test.gHappy("asd");
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testGHappyGInTheMiddleAlone(){
+        actual = test.gHappy("asdgasd");
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testGHappyGsInTheMiddle(){
+        actual = test.gHappy("asdggasd");
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testGHappyGsInTheMiddleMultipleTimes(){
+        actual = test.gHappy("asdggasgd");
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testGHappyGsInTheMiddleMultipleTimesTrue(){
+        actual = test.gHappy("asdggasggd");
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testGHappyThreeGsInTheMiddleAlone(){
+        actual = test.gHappy("asdgggasd");
+        assertTrue(actual);
+    }
+
+    @Test void testGHappyStartOfString(){
+        actual = test.gHappy("gasdd");
+        assertFalse(actual);
+    }
+
+    @Test void testGHappyGsStartOfString(){
+        actual = test.gHappy("ggasdd");
+        assertTrue(actual);
+    }
+
+    @Test void testGHappyGEndOfString(){
+        actual = test.gHappy("asddg");
+        assertFalse(actual);
+    }
+
+    @Test void testGHappyGsEndOfString(){
+        actual = test.gHappy("asddgg");
+        assertTrue(actual);
+    }
 }
